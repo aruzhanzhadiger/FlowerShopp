58f14cf003c34278506cae73488b2c9426e91d07
aruzhanzhadiger <aruzhan.zhadiger@icloud.com>
2026-01-24 08:10:33 +0500

    Remove IUserRepository interface

--- src/repositories/interfaces/IUserRepository.java
+++ src/repositories/interfaces/IUserRepository.java
@@ -1,11 +1,1 @@
-package repositories.interfaces;
 
-import models.User;
-
-import java.util.List;
-
-public interface IUserRepository {
-    boolean createUser(User user);
-    User getUser(int id);
-    List<User> getAllUsers();
-}
