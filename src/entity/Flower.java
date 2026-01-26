5d9481e416656d8afd8ae307ae3d38bfb3458f50
aruzhanzhadiger <aruzhan.zhadiger@icloud.com>
2026-01-26 16:11:46 +0500

    Refactor Flower class to include stock and double price
    Updated Flower class to use double for price and added stock attribute.

--- src/entity/Flower.java
+++ src/entity/Flower.java
@@ -1,17 +1,52 @@
+package entity;
+
 public class Flower {
     private int id;
     private String name;
-    private int price;
+    private double price;
+    private int stock;
 
-    public Flower(int id, String name, int price) {
+    public Flower(int id, String name, double price, int stock) {
+        this(name, price, stock);
+
+        setId(id);
+    }
+
+    public Flower(String name, double price, int stock) {
+        setName(name);
+        setPrice(price);
+        setStock(stock);
+    }
+
+    public int getId() { return id; }
+
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
-    
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public int getPrice() { return price;  }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 
     @Override
     public String toString() {
