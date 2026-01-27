633b58e874e8d9fa689dbf40b5498b3c9f729b62
aruzhanzhadiger <aruzhan.zhadiger@icloud.com>
2026-01-27 15:01:02 +0500

    Refactor addFlower method with Flower object

--- src/service/FlowerService.java
+++ src/service/FlowerService.java
@@ -1,3 +1,8 @@
+package service;
+
+import entity.Flower;
+import repositories.FlowerRepository;
+
 import java.sql.SQLException;
 import java.util.List;
 
@@ -8,12 +13,12 @@ public class FlowerService {
         this.repo = repo;
     }
 
-    public void addFlower(String name, double price) throws SQLException {
-        if (price == 0) {
+    public void createFlower(Flower flower) throws SQLException {
+        if (flower.getPrice() == 0) {
             System.out.println("Price must be more than 0");
             return;
         }
-        repo.addFlower(name, price);
+        repo.addFlower(flower);
     }
 
     public List<Flower> listFlowers() throws SQLException {
