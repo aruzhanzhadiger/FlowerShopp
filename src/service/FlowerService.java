cb4a7e0acb0ab5f83c5715cbe831ce8721b1ff23
aruzhanzhadiger <aruzhan.zhadiger@icloud.com>
2026-02-04 21:19:21 +0500

    Implement buyFlower method in FlowerService
    Added buyFlower method to handle flower purchases with discount logic.

--- src/service/FlowerService.java
+++ src/service/FlowerService.java
@@ -1,5 +1,6 @@
 package service;
 
+import entity.Buyer;
 import entity.Flower;
 import repositories.FlowerRepository;
 
@@ -23,5 +24,34 @@ public class FlowerService {
 
     public List<Flower> listFlowers() throws SQLException {
         return repo.getALLFlowers();
+    }
+
+    public double buyFlower(Buyer buyer, int flowerId, int quantity) throws SQLException {
+        Flower flower = repo.getFlowerById(flowerId);
+
+        if (flower == null) {
+            System.out.println("Flower not found");
+            return 0;
+        }
+
+        if (quantity > flower.getStock()) {
+            System.out.println("Not enough flowers in stock");
+            return 0;
+        }
+
+        double total = flower.getPrice() * quantity;
+        buyer.increasePurchases();
+
+        if (buyer.getPurchases() % 11 == 0) {
+            total *= 0.9;
+            System.out.println("You received a 10% discount for your 11th purchase!");
+        }
+
+        repo.updateStock(flowerId, flower.getStock() - quantity);
+        return total;
+    }
+
+    public void deleteFlower(int id) throws SQLException {
+        repo.deleteFlower(id);
     }
 }
