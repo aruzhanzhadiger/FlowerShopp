a334223cf61b801198df15ccb855a4d827ca2f31
aruzhanzhadiger <aruzhan.zhadiger@icloud.com>
2026-02-04 21:20:01 +0500

    Improve formatting of PostgresDB initialization
    Refactor database connection initialization for better readability.

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,16 @@ import data.interfaces.IDB;
 import entity.Flower;
 import repositories.FlowerRepository;
 import service.FlowerService;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            IDB db = new PostgresDB("jdbc:postgresql://localhost:5432", "postgres", "0000", "FlowerShop");
+            IDB db = new PostgresDB("jdbc:postgresql://localhost:5432",
+                    "postgres",
+                    "0000",
+                    "FlowerShop");
 
             Connection conn = db.getConnection();
 
