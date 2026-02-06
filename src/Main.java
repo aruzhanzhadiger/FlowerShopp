a0e7e03a8c9b761ec8c906c12df6bf4f11387560
aruzhanzhadiger <aruzhan.zhadiger@icloud.com>
2026-02-06 21:43:26 +0500

    Refactor Main class to include CategoryRepository

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,67 @@
 import controllers.FlowerController;
 import data.PostgresDB;
 import data.interfaces.IDB;
-import entity.Flower;
+import repositories.CategoryRepository;
 import repositories.FlowerRepository;
 import service.FlowerService;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            IDB db = new PostgresDB("jdbc:postgresql://localhost:5432",
+            IDB db = PostgresDB.getInstance(
+                    "jdbc:postgresql://localhost:5432",
                     "postgres",
                     "0000",
-                    "FlowerShop");
+                    "FlowerShop"
+            );
 
             Connection conn = db.getConnection();
 
-            FlowerRepository repo = new FlowerRepository(conn);
-            FlowerService service = new FlowerService(repo);
-            FlowerController controller = new FlowerController(service);
+            FlowerRepository flowerRepo = new FlowerRepository(conn);
+            CategoryRepository categoryRepo = new CategoryRepository(conn);
+
+            FlowerService service = new FlowerService(flowerRepo);
+            FlowerController controller = new FlowerController(service, categoryRepo);
 
             controller.start();
+
+            db.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}import controllers.FlowerController;
+import data.PostgresDB;
+import data.interfaces.IDB;
+import repositories.CategoryRepository;
+import repositories.FlowerRepository;
+import service.FlowerService;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            IDB db = PostgresDB.getInstance(
+                    "jdbc:postgresql://localhost:5432",
+                    "postgres",
+                    "0000",
+                    "FlowerShop"
+            );
+
+            Connection conn = db.getConnection();
+
+            FlowerRepository flowerRepo = new FlowerRepository(conn);
+            CategoryRepository categoryRepo = new CategoryRepository(conn);
+
+            FlowerService service = new FlowerService(flowerRepo);
+            FlowerController controller = new FlowerController(service, categoryRepo);
+
+            controller.start();
+
+            db.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
