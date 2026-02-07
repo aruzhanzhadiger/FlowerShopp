29d3bb1f397c80c72eca7f28aa027a322fb82059
gulnazkuatova <254305@astanait.edu.kz>
2026-02-07 07:21:12 +0500

    Update PostgresDB.java

--- src/data/PostgresDB.java
+++ src/data/PostgresDB.java
@@ -1,81 +1,46 @@
 package data;
+
 import data.interfaces.IDB;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class PostgresDB implements IDB {
-    private String host;
-    private String username;
-    private String password;
-    private String dbName;
-
+    private static PostgresDB instance;
+    private final String url;
+    private final String username;
+    private final String password;
     private Connection connection;
 
-    public PostgresDB(String host, String username, String password, String dbName) {
-        setHost(host);
-        setUsername(username);
-        setPassword(password);
-        setDbName(dbName);
-    }
-
-    @Override
-    public Connection getConnection() {
-        String connectionUrl = host + "/" + dbName;
-        try {
-            if (connection != null && !connection.isClosed()) {
-                return connection;
-            }
-
-            Class.forName("org.postgresql.Driver");
-
-            connection = DriverManager.getConnection(connectionUrl, username, password);
-
-            return connection;
-        } catch (Exception e) {
-            System.out.println("failed to connect to postgres: " + e.getMessage());
-
-            return null;
-        }
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
+    private PostgresDB(String hostUrl, String username, String password, String dbName) {
+        this.url = hostUrl + "/" + dbName;
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getDbName() {
-        return dbName;
-    }
-
-    public void setDbName(String dbName) {
-        this.dbName = dbName;
-    }
-
-    public void close() {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException ex) {
-                System.out.println("Connection close error: " + ex.getMessage());
-            }
+    public static synchronized PostgresDB getInstance(String hostUrl, String username, String password, String dbName) {
+        if (instance == null) {
+            instance = new PostgresDB(hostUrl, username, password, dbName);
         }
+        return instance;
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) return connection;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("PostgreSQL Driver not found. Add postgresql jar to project libraries.", e);
+        }
+
+        connection = DriverManager.getConnection(url, username, password);
+        return connection;
+    }
+
+    @Override
+    public void close() throws SQLException {
+        if (connection != null && !connection.isClosed()) connection.close();
     }
 }
